baeeca7c36ea808db32a0f33169c2e626b3fa49c
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-27 20:12:14 +0530

    exportar tudo no final do arquivo

--- import_export/main.js
+++ import_export/main.js
@@ -1,4 +1,10 @@
-import { nome, soma, PI } from './modulo.js'
-console.log(nome)
+// import { nome, soma, PI } from './modulo.js'
+import { soma, subtrai } from './modulo.js'
 console.log(soma(6, 3))
-console.log(PI)
+console.log(subtrai(7, 3))
+// console.log(nome)
+// console.log(soma(6, 3))
+// console.log(PI)
+
+// import { saudar } from './modulo.js'
+// console.log(`Boa tarde chamo-me ${saudar}`)
--- import_export/modulo.js
+++ import_export/modulo.js
@@ -1,3 +1,11 @@
-export const nome = "joaquim"
-export const soma = (a,b) => a + b;
-export const PI = 3.14157
+// export const nome = "joaquim"
+// export const soma = (a,b) => a + b;
+// export const PI = 3.14157
+export const saudar = 'joaquim'
+
+// exportar tudo no final do arquivo:
+const soma = (a, b) => a + b;
+const subtrai = (a, b) => a - b;
+
+export { soma, subtrai };
+
