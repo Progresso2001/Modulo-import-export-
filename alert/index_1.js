5a7a209b726c83e9da7d46fb7dad80747fc5bb80
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-28 00:33:54 +0530

    tratamento de erro com indice invaalido e export / import

--- alert/index_1.js
+++ alert/index_1.js
@@ -1,5 +1,22 @@
 import { clientDados } from './index.js'
 
-console.log("Nome do cliente: ", clientDados.nome)
-console.log("Produtos comprados: ", clientDados.produtos_comprado[0])
+const resClientes = clientDados.nome 
+const prodComprados = clientDados.produtos_comprado[6]
+console.log("Nome cliente: ", resClientes)
+
+// tratamento de erro com indice invaalido
+if(prodComprados === undefined){
+    console.log("Verificar o indice")
+}else{
+console.log("Produto comprado: ", prodComprados)
+}
+
+
+
+
+
+
+
+// console.log("Nome do cliente: ", clientDados.nome)
+// console.log("Produtos comprados: ", clientDados.produtos_comprado[0])
 
