27d1fb9e958694817a145ad364e0ba3b181e07e4
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-28 00:19:46 +0530

    praticando com objectos

---
+++ alert/index.js
@@ -0,0 +1,10 @@
+export const clientDados = {
+    nome: "DevJocas",
+    idade: 67,
+    cidade: "Luanda-Angola",
+    produtos_comprado: [
+        "NoteBook", "Mouse", "Auricular", "Router"
+    ]
+}
+
+// export const nome = "joaquim"
---
+++ alert/index_1.js
@@ -0,0 +1,5 @@
+import { clientDados } from './index.js'
+
+console.log("Nome do cliente: ", clientDados.nome)
+console.log("Produtos comprados: ", clientDados.produtos_comprado[0])
+
