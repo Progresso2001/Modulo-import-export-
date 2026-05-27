54d8fd669a2811fcdd08b1fe9206eabff9bd8652
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-27 19:59:16 +0530

    Usando modulos importe export

---
+++ import_export/main.js
@@ -0,0 +1,4 @@
+import { nome, soma, PI } from './modulo.js'
+console.log(nome)
+console.log(soma(6, 3))
+console.log(PI)
---
+++ import_export/modulo.js
@@ -0,0 +1,3 @@
+export const nome = "joaquim"
+export const soma = (a,b) => a + b;
+export const PI = 3.14157
