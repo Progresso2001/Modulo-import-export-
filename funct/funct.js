51bbf4a35051c40e5087a22375b864e46ac78f2c
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-28 15:47:04 +0530

    usando diferentes formas de export e import

---
+++ funct/funct.js
@@ -0,0 +1,7 @@
+export function dizerNome(){
+    return `Ola pessoa eu chamo-me DevJocas`
+}
+export const fazerOque  = 'Estudar'
+
+const qualLinguageEstudas =  'JavaScript'
+export default qualLinguageEstudas;
---
+++ funct/funct_1.js
@@ -0,0 +1,10 @@
+import  tipolinguagem, { dizerNome, fazerOque } from "./funct.js";
+
+const nomeDito= dizerNome()
+console.log(nomeDito);
+const estou= fazerOque
+console.log("estou a estudar programação: ", estou);
+console.log("Estou a aprendendo: ",tipolinguagem);
+
+
+
