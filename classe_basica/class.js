c35e168f76e6f968ec1e970507afe01951772666
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-28 20:40:40 +0530

    usando a exportação no final E chamar o metodo

--- classe_basica/class.js
+++ classe_basica/class.js
@@ -12,3 +12,20 @@ export default class User{
         this.last=last
     }
 }
+
+// Alternativa: Exportar no final do arquivo:
+
+class Pessoa{
+    constructor(nome, sobrenome, idade){
+        this.nome=nome
+        this.sobrenome=sobrenome
+        this.idade=idade
+
+    }
+    // usando um metodo
+    falar() {
+        console.log(`Nome: ${this.nome} ${this.sobrenome}, Idade: ${this.idade}`);
+    }
+}
+export { Pessoa }
+
--- classe_basica/class_1.js
+++ classe_basica/class_1.js
@@ -2,9 +2,14 @@ import { User } from "./class.js";
 const user = new User('Ana', 'Pedro')
 console.log(`Olá chamo-me ${user.first} ${user.last} `)
 
-// Usando default
+// // Usando default
 import { default  as User } from "./class.js"
 
 const nomePadrao = new User('Ana', 'Pedro')
 console.log(nomePadrao)
 console.log(`Olá chamo-me ${nomePadrao.first} ${nomePadrao.last} `)
+
+// usando a exportação no final E chamar o metodo
+import { Pessoa } from "./class.js";
+const pessoa = new Pessoa('Ana', 'Pedro', 13)
+pessoa.falar()
