ff8252e22ae59a8616668593be644cf4306d4b2b
Progresso2001 <joaquimmenianga99@gmail.com>
2026-05-29 16:51:45 +0530

    exemplos do uso do reduce

--- Arrays_methods/reduce.js
+++ Arrays_methods/reduce.js
@@ -23,3 +23,48 @@ const porCategoria = produtos.reduce((acc, produto)=>{
     return acc
 }, {})
 console.log("Nova categoria selecionada: ", porCategoria)
+
+// Filtar uma condição usando reduce()
+
+const pessoas =[
+    {nome:'Ana', idade:16},
+    {nome:'Pedro', idade:26},
+    {nome:'Lian', idade:19},
+    {nome:'joaquim', idade:20}
+]
+const adultos = pessoas.reduce((acc, adultosAtual)=>{
+    if(adultosAtual.idade >= 18){
+        acc.push(adultosAtual)
+    }
+    return acc
+}, [])
+console.log(adultos)
+
+// transformar um array em outro tipo de estrutura de dados
+
+const colors = ['red', 'green', 'blue'];
+  const hexCodes = colors.reduce((acc, curr) => {
+  switch (curr) {
+          case 'red':
+          acc[curr] = '#FF0000';
+          break;
+          case 'green':
+          acc[curr] = '#00FF00';
+          break;
+          case 'blue':
+          acc[curr] = '#0000FF';
+          break;
+      }
+  return acc;
+  }, {});
+  
+  console.log(hexCodes);
+
+// Exemplos basico
+const valores = [1, 3, 5, 6]
+const valorIncial = 2
+
+const resSoma = valores.reduce((acc, curr) =>{
+    return acc + curr
+}, valorIncial)
+console.log(resSoma)
