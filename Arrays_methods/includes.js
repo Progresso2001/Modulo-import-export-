2b04b88ca8a053a88af0f7f85a7a696c1d79f788
Progresso2001 <joaquimmenianga99@gmail.com>
2026-06-01 18:29:46 +0530

    usando o includes

---
+++ Arrays_methods/includes.js
@@ -0,0 +1,9 @@
+// usando o includes
+
+const zooAnimals = ['lion', 'tiger', 'snake', 'zebra']
+const temZebra = zooAnimals.includes('zebra')
+console.log(temZebra)
+// usando o if
+if(zooAnimals.includes('zebra')){
+    console.log("Contém animal: ", zooAnimals[7])
+}
