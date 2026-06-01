fe913446b72e03c0148bad12f132063544464139
Progresso2001 <joaquimmenianga99@gmail.com>
2026-06-01 19:59:19 +0530

    usando o metdod join

---
+++ Arrays_methods/join.js
@@ -0,0 +1,12 @@
+const frutas = ["Manga", "Banana", "Pera"]
+const juntarPor = frutas.join('-')
+console.log(juntarPor)
+
+const letras = ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"];
+const palavra = letras.join("")
+console.log(palavra)
+
+const numeros = [2024, 1, 15];
+const data = numeros.join("/");
+
+console.log(data);
