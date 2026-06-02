54b2f7b2fd3c15364412017f9e0b93482db22378
Progresso2001 <joaquimmenianga99@gmail.com>
2026-06-02 13:38:14 +0530

    usando objecto de array

--- object/index.js
+++ object/index.js
@@ -1,10 +1,10 @@
 const person ={
     name: 'DevJocas', 
     city: 'Luanda',
-    skill_curse : {
+    skill_curse : [{
         curse_1: 'JavaScript', 
         curse_2: 'Python',
         curse_3: 'MySQL'
-    }
+    }]
 }
 export { person } ;
